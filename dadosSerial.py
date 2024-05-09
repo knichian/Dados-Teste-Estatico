d87cc1eee93d2e12f633e19c589d4e4c4456b99c
Rocketry <136114372+SerraRocketry@users.noreply.github.com>
2024-05-09 07:47:44 -0300

    Add files via upload

---
+++ dadosSerial.py
@@ -0,0 +1,17 @@
+import serial #Importa a biblioteca
+
+while True: #Loop para a conexão com o Arduino
+    try:  #Tenta se conectar, se conseguir, o loop se encerra
+        com = serial.Serial('/dev/ttyUSB0', 115200)
+        print('Arduino conectado')
+        break
+    except:
+        pass
+
+while True: #Loop principal
+    msg = str(com.readline()) #Lê os dados em formato de string
+    msg = msg[2:-5] #Fatia a string
+    print(msg) #Imprime a mensagem
+    with open('/home/viniciusmonnerat/Documentos/Serra_Rocketry/Analise de dados/Dados.txt', 'a') as file:
+        file.write(msg + '\n')
+    com.flush() #Limpa a comunicação
