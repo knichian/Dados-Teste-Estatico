b3c9d825ead6dbdf05bf26015befd822beb73e3d
Rocketry <136114372+SerraRocketry@users.noreply.github.com>
2024-05-09 08:17:38 -0300

    Add files via upload

---
+++ calibrar.py
@@ -0,0 +1,17 @@
+data = []
+arq = open('Analise de dados/Dados.txt', 'r').read()
+lines = arq.split('\n')
+for line in lines:
+    data.append(line)
+
+soma = 0
+for i in range(len(data)):
+    soma+=float(data[i])
+
+media = soma/len(data)
+
+peso = int(input('Digite o peso real do objeto: '))
+
+calibracao = media/peso
+
+print('O fator de calibração é: ', calibracao)
