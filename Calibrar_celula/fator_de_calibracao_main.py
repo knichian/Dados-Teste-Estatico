f2657750b2c97653e6efa95af3b167c16598a4b8
Gabriel <knichian@gmail.com>
2025-09-23 02:04:09 -0300

    Adicionando a funcionalidade de atualizar a lista de dispositivos seriais

--- Calibrar_celula/fator_de_calibracao_main.py
+++ Calibrar_celula/fator_de_calibracao_main.py
@@ -15,6 +15,22 @@ from ui_fator_de_calibracao import Ui_MainWindow
 
 import sys
 
+
+def extend_ui(ui):
+
+    def update_port_list():
+        ports = list_ports()
+        ui.dispositivoSerialComboBox.clear()
+        ui.dispositivoSerialComboBox.addItems(['dummy-data-4', 'dummy-data-5', 'dummy-data-6',]) # TODO: remove later
+        ui.dispositivoSerialComboBox.addItems(ports)
+
+    ports = list_ports()
+    ui.dispositivoSerialComboBox.addItems(['dummy-data-1', 'dummy-data-2', 'dummy-data-3',]) # TODO: remove later
+    ui.dispositivoSerialComboBox.addItems(ports)
+    
+    ui.pushButton_4.clicked.connect(update_port_list)
+    
+
 if __name__ == '__main__':
 
     app = QApplication(sys.argv)
@@ -22,6 +38,7 @@ if __name__ == '__main__':
 
     ui = Ui_MainWindow()
     ui.setupUi(window)
+    extend_ui(ui)
 
     window.show()
     app.exec()
