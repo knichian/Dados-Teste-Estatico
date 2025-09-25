aadea2acb1100206390f56c8996089381e1cbae2
Rocketry-MX_Linux <null@void.com>
2025-09-25 17:24:44 -0300

    Ignorando __pycache__, reformulando UI & criando conecção ao ESP (broken state)

--- Calibrar_celula/fator_de_calibracao_main.py
+++ Calibrar_celula/fator_de_calibracao_main.py
@@ -10,26 +10,83 @@ from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QFrame,
     QMainWindow, QMenuBar, QPushButton, QSizePolicy,
     QSpacerItem, QStatusBar, QVBoxLayout, QWidget)
 
-from fator_de_calibracao_cli import *
+from fator_de_calibracao_cli import get_calibration_factor, list_ports, get_samples, Receiver
 from ui_fator_de_calibracao import Ui_MainWindow
 
 import sys
 
-
 def extend_ui(ui):
+
+    dummy_elements = False
+    global com
+    com = None
 
     def update_port_list():
         ports = list_ports()
-        ui.dispositivoSerialComboBox.clear()
-        ui.dispositivoSerialComboBox.addItems(['dummy-data-4', 'dummy-data-5', 'dummy-data-6',]) # TODO: remove later
-        ui.dispositivoSerialComboBox.addItems(ports)
+        ui.cbox_seriais.clear()
+        ui.cbox_seriais.addItems(['dummy-data-4', 'dummy-data-5', 'dummy-data-6',]) if dummy_elements else None 
+        ui.cbox_seriais.addItems(ports)
+
+    def get_samples_with_progress(com: Receiver, n_samples: int = 100):
+
+        print("get_sample_with_progress()") if dummy_elements else None 
+        samples = []
+        sample_count = 0
+
+        while(com.check_connection() and sample_count < n_samples):
+
+            # ui.display_status.text()
+            response = int(com.read_response())
+            samples.append(response)
+            sample_count+=1
+            print(f"Sample {sample_count}: {response}")
+            ui.progressBar.value = int(float(sample_count/n_samples)*100)
+
+        return samples
+
+    def connect_esp():
+
+        port = str(ui.cbox_seriais.currentText())
+        print(f"{port=}")
+        global com
+        com = Receiver(port)
+        print(f"{com=}")
+        com.send_command(b'INIT CONFIG\n')
+        ui.display_status.setText('Conectado')
+
+    def send_factor():
+        print(f"{com=}")
+        com.send_command(b'SET LOAD FACTOR ' + bytes(str(ui.line_edit_fator.text()).encode()) + b'\n')    
+        i = 0
+        while i < 10:
+           print(com.read_response()) 
+           i+=1
+
+    def calculate_factor():
+        print(f"{com=}")
+
+        if com == None:
+            return -1
+
+        samples = get_samples_with_progress(com)
+        expected_weight = ui.lineEdit_4.text()
+
+        if expected_weight == '':
+            return -1
+
+        calibration_factor = get_calibration_factor(samples, float(expected_weight))
+
+        ui.line_edit_fator.setText(str(calibration_factor))
 
     ports = list_ports()
-    ui.dispositivoSerialComboBox.addItems(['dummy-data-1', 'dummy-data-2', 'dummy-data-3',]) # TODO: remove later
-    ui.dispositivoSerialComboBox.addItems(ports)
+    ui.cbox_seriais.addItems(['dummy-data-1', 'dummy-data-2', 'dummy-data-3',]) if dummy_elements else None 
+    ui.cbox_seriais.addItems(ports)
     
-    ui.pushButton_4.clicked.connect(update_port_list)
-    
+    ui.btn_atualizar.clicked.connect(update_port_list)
+    ui.btn_calcular.clicked.connect(calculate_factor)
+    ui.btn_conectar.clicked.connect(connect_esp)
+    ui.btn_enviar.clicked.connect(send_factor)
+
 
 if __name__ == '__main__':
 
--- Calibrar_celula/ui_fator_de_calibracao.py
+++ Calibrar_celula/ui_fator_de_calibracao.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'fator_de_calibracaoRXQvey.ui'
+## Form generated from reading UI file 'fator_de_calibracaodrXDJb.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.9.2
 ##
@@ -17,16 +17,14 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QApplication, QComboBox, QFormLayout, QFrame,
     QGroupBox, QHBoxLayout, QLabel, QLineEdit,
-    QMainWindow, QPushButton, QSizePolicy, QSpacerItem,
-    QTabWidget, QVBoxLayout, QWidget)
+    QMainWindow, QProgressBar, QPushButton, QSizePolicy,
+    QSpacerItem, QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(698, 717)
-        MainWindow.setDocumentMode(False)
-        MainWindow.setTabShape(QTabWidget.TabShape.Rounded)
+        MainWindow.resize(701, 717)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
@@ -63,49 +61,110 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_2.addWidget(self.label_2, 0, Qt.AlignmentFlag.AlignHCenter)
 
+        self.frame_6 = QFrame(self.frame_1)
+        self.frame_6.setObjectName(u"frame_6")
+        self.frame_6.setFrameShape(QFrame.Shape.NoFrame)
+        self.frame_6.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_5 = QHBoxLayout(self.frame_6)
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+        self.label_6 = QLabel(self.frame_6)
+        self.label_6.setObjectName(u"label_6")
+        font1 = QFont()
+        font1.setPointSize(12)
+        self.label_6.setFont(font1)
+
+        self.horizontalLayout_5.addWidget(self.label_6)
+
+        self.line_edit_fator = QLineEdit(self.frame_6)
+        self.line_edit_fator.setObjectName(u"line_edit_fator")
+        self.line_edit_fator.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.line_edit_fator.setReadOnly(False)
+
+        self.horizontalLayout_5.addWidget(self.line_edit_fator)
+
+
+        self.verticalLayout_2.addWidget(self.frame_6)
+
         self.frame = QFrame(self.frame_1)
         self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.Shape.NoFrame)
         self.horizontalLayout_2 = QHBoxLayout(self.frame)
         self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.lineEdit = QLineEdit(self.frame)
-        self.lineEdit.setObjectName(u"lineEdit")
-        self.lineEdit.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.lineEdit.setReadOnly(False)
-
-        self.horizontalLayout_2.addWidget(self.lineEdit, 0, Qt.AlignmentFlag.AlignHCenter)
-
         self.pushButton_3 = QPushButton(self.frame)
         self.pushButton_3.setObjectName(u"pushButton_3")
 
         self.horizontalLayout_2.addWidget(self.pushButton_3)
 
+        self.btn_enviar = QPushButton(self.frame)
+        self.btn_enviar.setObjectName(u"btn_enviar")
 
-        self.verticalLayout_2.addWidget(self.frame, 0, Qt.AlignmentFlag.AlignHCenter)
+        self.horizontalLayout_2.addWidget(self.btn_enviar)
 
 
-        self.verticalLayout_4.addWidget(self.frame_1, 0, Qt.AlignmentFlag.AlignHCenter)
+        self.verticalLayout_2.addWidget(self.frame)
 
-        self.verticalSpacer = QSpacerItem(614, 68, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.verticalLayout_4.addItem(self.verticalSpacer)
+        self.verticalLayout_4.addWidget(self.frame_1, 0, Qt.AlignmentFlag.AlignHCenter|Qt.AlignmentFlag.AlignVCenter)
 
-        self.groupBox_4 = QGroupBox(self.frame_2)
-        self.groupBox_4.setObjectName(u"groupBox_4")
-        self.groupBox_4.setContextMenuPolicy(Qt.ContextMenuPolicy.DefaultContextMenu)
-        self.horizontalLayout_3 = QHBoxLayout(self.groupBox_4)
+        self.verticalSpacer_3 = QSpacerItem(20, 55, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout_4.addItem(self.verticalSpacer_3)
+
+        self.group_dispositivo_serial = QGroupBox(self.frame_2)
+        self.group_dispositivo_serial.setObjectName(u"group_dispositivo_serial")
+        self.group_dispositivo_serial.setContextMenuPolicy(Qt.ContextMenuPolicy.DefaultContextMenu)
+        self.verticalLayout_3 = QVBoxLayout(self.group_dispositivo_serial)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.frame_5 = QFrame(self.group_dispositivo_serial)
+        self.frame_5.setObjectName(u"frame_5")
+        self.frame_5.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_5.setFrameShadow(QFrame.Shadow.Sunken)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_5)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.label_4 = QLabel(self.frame_5)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setFont(font1)
+
+        self.horizontalLayout_4.addWidget(self.label_4, 0, Qt.AlignmentFlag.AlignHCenter)
+
+        self.display_status = QLabel(self.frame_5)
+        self.display_status.setObjectName(u"display_status")
+        font2 = QFont()
+        font2.setPointSize(12)
+        font2.setBold(True)
+        self.display_status.setFont(font2)
+
+        self.horizontalLayout_4.addWidget(self.display_status)
+
+
+        self.verticalLayout_3.addWidget(self.frame_5, 0, Qt.AlignmentFlag.AlignHCenter)
+
+        self.cbox_seriais = QComboBox(self.group_dispositivo_serial)
+        self.cbox_seriais.setObjectName(u"cbox_seriais")
+
+        self.verticalLayout_3.addWidget(self.cbox_seriais)
+
+        self.frame_4 = QFrame(self.group_dispositivo_serial)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame_4)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.dispositivoSerialComboBox = QComboBox(self.groupBox_4)
-        self.dispositivoSerialComboBox.setObjectName(u"dispositivoSerialComboBox")
+        self.btn_conectar = QPushButton(self.frame_4)
+        self.btn_conectar.setObjectName(u"btn_conectar")
 
-        self.horizontalLayout_3.addWidget(self.dispositivoSerialComboBox)
+        self.horizontalLayout_3.addWidget(self.btn_conectar)
 
-        self.pushButton_4 = QPushButton(self.groupBox_4)
-        self.pushButton_4.setObjectName(u"pushButton_4")
+        self.btn_atualizar = QPushButton(self.frame_4)
+        self.btn_atualizar.setObjectName(u"btn_atualizar")
 
-        self.horizontalLayout_3.addWidget(self.pushButton_4)
+        self.horizontalLayout_3.addWidget(self.btn_atualizar)
 
 
-        self.verticalLayout_4.addWidget(self.groupBox_4)
+        self.verticalLayout_3.addWidget(self.frame_4, 0, Qt.AlignmentFlag.AlignHCenter)
+
+
+        self.verticalLayout_4.addWidget(self.group_dispositivo_serial)
 
         self.groupBox_2 = QGroupBox(self.frame_2)
         self.groupBox_2.setObjectName(u"groupBox_2")
@@ -113,58 +172,58 @@ class Ui_MainWindow(object):
         self.groupBox_2.setFlat(False)
         self.formLayout = QFormLayout(self.groupBox_2)
         self.formLayout.setObjectName(u"formLayout")
-        self.label = QLabel(self.groupBox_2)
-        self.label.setObjectName(u"label")
-
-        self.formLayout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label)
-
-        self.lineEdit_3 = QLineEdit(self.groupBox_2)
-        self.lineEdit_3.setObjectName(u"lineEdit_3")
-
-        self.formLayout.setWidget(0, QFormLayout.ItemRole.FieldRole, self.lineEdit_3)
-
         self.label_3 = QLabel(self.groupBox_2)
         self.label_3.setObjectName(u"label_3")
+        self.label_3.setFont(font1)
 
-        self.formLayout.setWidget(2, QFormLayout.ItemRole.LabelRole, self.label_3)
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.LabelRole, self.label_3)
 
         self.lineEdit_4 = QLineEdit(self.groupBox_2)
         self.lineEdit_4.setObjectName(u"lineEdit_4")
 
-        self.formLayout.setWidget(2, QFormLayout.ItemRole.FieldRole, self.lineEdit_4)
-
-        self.espaOEntreAmostrasLabel = QLabel(self.groupBox_2)
-        self.espaOEntreAmostrasLabel.setObjectName(u"espaOEntreAmostrasLabel")
-
-        self.formLayout.setWidget(1, QFormLayout.ItemRole.LabelRole, self.espaOEntreAmostrasLabel)
-
-        self.espaOEntreAmostrasLineEdit = QLineEdit(self.groupBox_2)
-        self.espaOEntreAmostrasLineEdit.setObjectName(u"espaOEntreAmostrasLineEdit")
-
-        self.formLayout.setWidget(1, QFormLayout.ItemRole.FieldRole, self.espaOEntreAmostrasLineEdit)
+        self.formLayout.setWidget(0, QFormLayout.ItemRole.FieldRole, self.lineEdit_4)
 
 
-        self.verticalLayout_4.addWidget(self.groupBox_2)
+        self.verticalLayout_4.addWidget(self.groupBox_2, 0, Qt.AlignmentFlag.AlignVCenter)
 
-        self.verticalSpacer_3 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        self.verticalSpacer_4 = QSpacerItem(20, 55, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
 
-        self.verticalLayout_4.addItem(self.verticalSpacer_3)
+        self.verticalLayout_4.addItem(self.verticalSpacer_4)
+
+        self.verticalSpacer = QSpacerItem(20, 55, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout_4.addItem(self.verticalSpacer)
 
         self.frame_3 = QFrame(self.frame_2)
         self.frame_3.setObjectName(u"frame_3")
         self.frame_3.setFrameShape(QFrame.Shape.StyledPanel)
         self.frame_3.setFrameShadow(QFrame.Shadow.Raised)
-        self.horizontalLayout = QHBoxLayout(self.frame_3)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.pushButton = QPushButton(self.frame_3)
-        self.pushButton.setObjectName(u"pushButton")
+        self.verticalLayout_5 = QVBoxLayout(self.frame_3)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.frame_7 = QFrame(self.frame_3)
+        self.frame_7.setObjectName(u"frame_7")
+        self.frame_7.setFrameShape(QFrame.Shape.StyledPanel)
+        self.frame_7.setFrameShadow(QFrame.Shadow.Raised)
+        self.horizontalLayout_6 = QHBoxLayout(self.frame_7)
+        self.horizontalLayout_6.setObjectName(u"horizontalLayout_6")
+        self.btn_calcular = QPushButton(self.frame_7)
+        self.btn_calcular.setObjectName(u"btn_calcular")
 
-        self.horizontalLayout.addWidget(self.pushButton)
+        self.horizontalLayout_6.addWidget(self.btn_calcular)
 
-        self.pushButton_2 = QPushButton(self.frame_3)
+        self.pushButton_2 = QPushButton(self.frame_7)
         self.pushButton_2.setObjectName(u"pushButton_2")
 
-        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.horizontalLayout_6.addWidget(self.pushButton_2)
+
+
+        self.verticalLayout_5.addWidget(self.frame_7)
+
+        self.progressBar = QProgressBar(self.frame_3)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setValue(0)
+
+        self.verticalLayout_5.addWidget(self.progressBar)
 
 
         self.verticalLayout_4.addWidget(self.frame_3, 0, Qt.AlignmentFlag.AlignHCenter)
@@ -174,22 +233,13 @@ class Ui_MainWindow(object):
 
         MainWindow.setCentralWidget(self.centralwidget)
 #if QT_CONFIG(shortcut)
-        self.label_2.setBuddy(self.lineEdit)
-        self.label.setBuddy(self.lineEdit_3)
+        self.label_2.setBuddy(self.line_edit_fator)
         self.label_3.setBuddy(self.lineEdit_4)
-        self.espaOEntreAmostrasLabel.setBuddy(self.espaOEntreAmostrasLineEdit)
 #endif // QT_CONFIG(shortcut)
-        QWidget.setTabOrder(self.lineEdit, self.pushButton_3)
-        QWidget.setTabOrder(self.pushButton_3, self.dispositivoSerialComboBox)
-        QWidget.setTabOrder(self.dispositivoSerialComboBox, self.pushButton_4)
-        QWidget.setTabOrder(self.pushButton_4, self.lineEdit_3)
-        QWidget.setTabOrder(self.lineEdit_3, self.lineEdit_4)
-        QWidget.setTabOrder(self.lineEdit_4, self.pushButton)
-        QWidget.setTabOrder(self.pushButton, self.pushButton_2)
 
         self.retranslateUi(MainWindow)
-        self.pushButton_3.clicked.connect(self.lineEdit.selectAll)
-        self.pushButton_3.clicked.connect(self.lineEdit.copy)
+        self.pushButton_3.clicked.connect(self.line_edit_fator.selectAll)
+        self.pushButton_3.clicked.connect(self.line_edit_fator.copy)
         self.pushButton_2.clicked.connect(MainWindow.close)
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -198,16 +248,19 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Fator de Calibra\u00e7\u00e3o", None))
-        self.lineEdit.setText("")
-        self.lineEdit.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Vazio", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Resultado:", None))
+        self.line_edit_fator.setText("")
+        self.line_edit_fator.setPlaceholderText(QCoreApplication.translate("MainWindow", u"Vazio", None))
         self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Copiar", None))
-        self.groupBox_4.setTitle(QCoreApplication.translate("MainWindow", u"Dispositivo Serial: ", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Atualizar", None))
+        self.btn_enviar.setText(QCoreApplication.translate("MainWindow", u"Enviar", None))
+        self.group_dispositivo_serial.setTitle(QCoreApplication.translate("MainWindow", u"Dispositivo Serial: ", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Status de Conex\u00e3o:", None))
+        self.display_status.setText(QCoreApplication.translate("MainWindow", u"Desconectado", None))
+        self.btn_conectar.setText(QCoreApplication.translate("MainWindow", u"Conectar", None))
+        self.btn_atualizar.setText(QCoreApplication.translate("MainWindow", u"Atualizar", None))
         self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"Ajustes: ", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"Quantidade de Amostras: ", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Peso Real: ", None))
-        self.espaOEntreAmostrasLabel.setText(QCoreApplication.translate("MainWindow", u"Espa\u00e7o entre Amostras", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Calcular", None))
+        self.btn_calcular.setText(QCoreApplication.translate("MainWindow", u"Calcular", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Fechar", None))
     # retranslateUi
 
